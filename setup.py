2273d93ded2c9835bb54328d4939685abae423f7
Christoforou Manolis <christoforou712@gmail.com>
2019-01-08 15:44:58 +0200

    imcrease version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pycoingecko',
-    version='0.1.0',
+    version='0.1.1',
     packages=['pycoingecko',],
     license='MIT',
     description = 'Python wrapper around the CoinGecko API',
