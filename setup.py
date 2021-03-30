656f0a929aa65d888ec1c9aa4d1b524314862f46
Manolis Christoforou <emchristoforou@gmail.com>
2021-03-30 15:11:18 +0300

    updated version and changelog

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='pycoingecko',
-    version='1.4.0',
+    version='1.4.1',
     packages=['pycoingecko',],
     license='MIT',
     description = 'Python wrapper around the CoinGecko API',
